0d787834cb5f6c6d8f5d2a66fb8c9910edbadee3
fernando <fernandomk6@gmail.com>
2022-11-02 18:11:36 -0300

    Concluído aplicação

--- js/app.js
+++ js/app.js
@@ -1,1 +1,84 @@
-// manipular o dom e exibir as info na tela
+const formChangeLocation = document.querySelector('[data-js="change-location"]')
+const h5CityLocation = document.querySelector('[data-js="city-location"]')
+const spanTemperature = document.querySelector('[data-js="temperature"]')
+const divWeatherText = document.querySelector('[data-js="weather-text"]')
+const imgTime = document.querySelector('[data-js="time"]')
+const divTimeIcon = document.querySelector('[data-js="time-icon"]')
+const divWeatherCard = document.querySelector('[data-js="weather-card"]')
+const spanLoading = document.querySelector('[data-js="loading"]')
+
+const setImgTime = (isDayTime) => {
+  const time = isDayTime ? 'day' : 'night'
+  imgTime.setAttribute('src', `./src/${time}.svg`)
+  imgTime.setAttribute('alt', `${time}`)
+}
+
+const setTimeIcon = (weatherIcon) => {
+  const imgTimeIconHTML = `<img src="./src/icons/${weatherIcon}.svg"/>`
+  divTimeIcon.innerHTML = imgTimeIconHTML
+}
+
+const showWeatherCard = () => {
+  const isVisible = !divWeatherCard.classList.contains('d-none')
+  !isVisible && divWeatherCard.classList.remove('d-none')
+}
+
+const buildWeatherCard = async (weatherCardInfo) => {
+  const { 
+    cityName,
+    temperature,
+    weatherText,
+    isDayTime,
+    weatherIcon,
+    state,
+    country
+  } = await weatherCardInfo
+  
+  const cityLocation = `${cityName}, ${state} - ${country}`
+  h5CityLocation.textContent = cityLocation
+  spanTemperature.textContent = temperature
+  divWeatherText.textContent = weatherText
+
+  setImgTime(isDayTime)
+  setTimeIcon(weatherIcon)
+}
+
+const getWeatherCardInfo = async (cityName) => {
+  showLoadingMessage('Buscando clima na cidade')
+
+  try {
+    const [{ Key, LocalizedName, AdministrativeArea, Country }] = await getCityData(cityName)
+    const [{ IsDayTime, Temperature, WeatherIcon, WeatherText }] = await getCityWeatherData(Key)
+
+    return {
+      cityName: LocalizedName,
+      temperature: Temperature.Metric.Value,
+      weatherText: WeatherText,
+      isDayTime: IsDayTime,
+      weatherIcon: WeatherIcon,
+      country: Country.LocalizedName, 
+      state: AdministrativeArea.ID
+    }
+  } catch (error) {
+    console.log(error.message)
+    showLoadingMessage('Cidade não encontrada')
+  }
+}
+
+const showLoadingMessage = (message) => {
+  spanLoading.textContent = message || '' 
+}
+
+formChangeLocation.addEventListener('submit', async (event) => {
+  event.preventDefault()
+
+  const cityName = event.target.city.value
+  const weatherCardInfo = getWeatherCardInfo(cityName)
+
+  await buildWeatherCard(weatherCardInfo)
+
+  showLoadingMessage(false)
+  showWeatherCard()
+
+  event.target.reset()
+})
--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,33 @@
-// codigo responsavel por iteragir com a api
+const APIKey = '0BEDxrdgdklT5SzYqs477akGAsA6Fteb'
+const baseURL = 'http://dataservice.accuweather.com'
+const language = 'pt-br'
+
+const getCityData = async (cityName) => {
+  try {
+    const response = await fetch(`${baseURL}/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}&language=${language}`)
+
+    if (!response.ok) {
+      throw new Error('Não foi possível obter os dados da cidade')
+    }
+
+    return response.json()
+    
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+const getCityWeatherData = async (locationKey) => {
+  try {
+    const response = await fetch(`${baseURL}/currentconditions/v1/${locationKey}?apikey=${APIKey}&language=${language}`)
+
+    if (!response.ok) {
+      throw new Error('Não foi possível obter os dados do clima')
+    }
+
+    return response.json()
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
